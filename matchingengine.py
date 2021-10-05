19fdbbb93e0200d7b8fa77b65d3728d1f9bd3590
bartlp20 <a11946646@unet.univie.ac.at>
2021-10-05 15:58:59 +0200

    first constructs

---
+++ matchingengine.py
@@ -0,0 +1,76 @@
+import sortedcontainers
+
+
+class Order:
+    def __init__(self, time, orderId, side, price, quant):
+        self.time = time
+        self.orderId = orderId
+        self.side = side
+        self.price = price
+        self.quant = quant
+
+
+class Orderbook:
+    def __init__(self, bids=[], asks=[]):
+        self.bids = sortedcontainers.SortedList(
+            bids, key=lambda order: -order.price)  # buy orders
+        self.asks = sortedcontainers.SortedList(
+            bids, key=lambda order: order.price)  # sell orders
+
+    def printBook(self):
+        print("ORDER BOOK")
+        print("BIDS\nQuant Price")
+        for bid in self.bids:
+            print(str(bid.quant) + " " + str(bid.price))
+
+        print("ASKS\nQuant Price")
+        for ask in self.asks:
+            print(str(ask.quant) + " " + str(ask.price))
+
+    def addOrder(self, order):
+        if order.side == "B":
+            self.bids.add(order)
+        elif order.side == "S":
+            self.asks.add(order)
+
+    def removeOrder(self, orderId, quant):
+        # find order with id then adjust quant
+        # if quant is zero, remove order completely
+        if not self.removeOrder_(self.bids, orderId, quant):
+            if not self.removeOrder_(self.asks, orderId, quant):
+                print("didn't delete nothing")
+
+    def removeOrder_(self, liste, orderId, quant):
+        for order in liste:
+            if order.orderId == orderId:
+                index = liste.index(order)
+                if liste[index].quant == quant:
+                    print("removing order at index: {}".format(str(index)))
+                    liste.pop(index)
+                    return True
+                elif liste[index].quant > quant:
+                    print("adjusting order at index: {}".format(str(index)))
+                    liste[index].quant -= quant
+                    return True
+                else:
+                    return False
+
+    def getSpread(self):
+        return self.asks[0].price - self.bids[0].price
+
+    def getHighestBid(self):
+        if len(self.bids) == 0:
+            return 0
+        else:
+            return self.bids[0].price
+
+    def getLowestAsk(self):
+        if len(self.asks) == 0:
+            return 0
+        else:
+            return self.asks[0].price
+
+    def match(self):
+        # check if spread passt
+        # sell order preis muss kleiner gleich sein als lowest ask
+        pass
